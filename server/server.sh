490312f30d340ff0d1964c425e1e3e61c618ded9
nesjett <snestor4eso@gmail.com>
2017-04-22 15:19:40 +0200

    created config file and server launch script

---
+++ server/server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+####################################
+#
+# Launch web server
+#
+####################################
+
+
+# Print start status message.
+echo "Launching web server"
+date
+echo
+
+
+./mi_httpd
+
+
